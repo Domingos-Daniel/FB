4a1d3279a741d7b21f11e3015a14ce3f2b6256d4
Domingos Daniel <domingoscahandadaniel@gmail.com>
2024-04-14 22:14:14 +0100

    Testando Approvals by Eighty

--- app/Filament/Clusters/Programas/Resources/OrcamentoResource/Pages/EditOrcamento.php
+++ app/Filament/Clusters/Programas/Resources/OrcamentoResource/Pages/EditOrcamento.php
@@ -3,11 +3,14 @@
 namespace App\Filament\Clusters\Programas\Resources\OrcamentoResource\Pages;
 
 use App\Filament\Clusters\Programas\Resources\OrcamentoResource;
+use EightyNine\Approvals\Models\ApprovableModel;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Actions\Action;
 
 class EditOrcamento extends EditRecord
 {
+    use  \EightyNine\Approvals\Traits\HasApprovalHeaderActions;
     protected static string $resource = OrcamentoResource::class;
 
     protected function getHeaderActions(): array
@@ -16,5 +19,20 @@ class EditOrcamento extends EditRecord
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    /**
+     * Get the completion action.
+     *
+     * @return Filament\Actions\Action
+     * @throws Exception
+     */
+    protected function getOnCompletionAction(): Action
+    {
+        return Action::make("Done")
+            ->color("success")
+            // Do not use the visible method, since it is being used internally to show this action if the approval flow has been completed.
+            // Using the hidden method add your condition to prevent the action from being performed more than once
+            ->hidden(fn(ApprovableModel $record)=> $record->shouldBeHidden());
     }
 }
